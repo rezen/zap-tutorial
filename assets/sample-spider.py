0247f3eedf7f88fe0b31155c13f66a0e204b99b9
Andres Hermosilla <an2dres5m@gmail.com>
2019-02-20 13:58:47 -0800

    Added KeyboardInterrupt for the sample-spider.py

--- assets/sample-spider.py
+++ assets/sample-spider.py
@@ -11,17 +11,23 @@ print("Spidering target %s" % target)
 
 zap = ZAPv2()
 
-zap.spider.exclude_from_scan(".*node_modules.*")
-zap.spider.exclude_from_scan("%s/public.*" % target)
-zap.spider.exclude_from_scan("%s/i18n.*" % target)
+zap.spider.exclude_from_scan(".*/node_modules/.*")
+zap.spider.exclude_from_scan(".*/css/.*")
+zap.spider.exclude_from_scan("%s/public/.*" % target)
+zap.spider.exclude_from_scan("%s/dist/.*" % target)
+zap.spider.exclude_from_scan("%s/private/.*" % target)
+zap.spider.exclude_from_scan("%s/i18n/.*" % target)
 
-scan_id = zap.spider.scan(url=target, maxchildren=2, recurse=True, contextname=None, subtreeonly=None)
+scan_id = zap.spider.scan(url=target)
+time.sleep(0.1)
 
-time.sleep(2)
+try:
+    while (int(zap.spider.status(scan_id)) < 100):
+        print("Spider progress " + zap.spider.status(scan_id) + "%")
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    zap.spider.stop(scan_id)
 
-while (int(zap.spider.status(scan_id)) < 100):
-  print("Spider progress " + zap.spider.status(scan_id) + "%")
-  time.sleep(2)
 
 print("Spider scan completed")
 zap.spider.clear_excluded_from_scan()
